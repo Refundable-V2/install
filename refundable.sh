70eaafd881523953a1af7f6d3e249fd5ab88c65b
dstaringer <43230937+dstaringer@users.noreply.github.com>
2022-04-05 03:46:41 +0200

    docker-compose version changed

--- refundable.sh
+++ refundable.sh
@@ -34,7 +34,7 @@ sub_install() {
     rm docker-install.sh
     printf "\n------\n\n"
     printf "Downloading Docker-Compose.\n"
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     printf "Download complete.\n"
     sudo chmod +x /usr/local/bin/docker-compose
     printf "Docker and Docker Compose installed successfully.\n"
